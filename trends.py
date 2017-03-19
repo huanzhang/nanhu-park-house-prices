32e12b7596d5c7a7543c242034f8eba8fe78c724
Huan Zhang <huan_zh@yahoo.com>
2017-03-19 23:08:07 +0800

    plotting...

---
+++ trends.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+PRICES_DATA = pd.read_csv("house_prices.csv", parse_dates=['date'])
+
+
+if __name__ == "__main__":
+    house_prices = PRICES_DATA[['price','date']]
+    house_prices = house_prices.set_index('date')
+    house_prices.resample('M',how=np.mean)
+
+    house_prices_plot = house_prices.plot(title="House Price by Month",legend=None)
+
+    fig = house_prices_plot.get_figure()
+    fig.savefig("out.png")
