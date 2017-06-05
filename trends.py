0c0b5b8d3c72bf2674994b41a69087f4cb2cb930
Zhang Huan <zhang_huan@yahoo.com>
2017-06-05 18:05:07 +0800

    update house prices.csv

--- trends.py
+++ trends.py
@@ -9,7 +9,8 @@ PRICES_DATA = pd.read_csv("house_prices.csv", parse_dates=['date'])
 if __name__ == "__main__":
     house_prices = PRICES_DATA[['price','date']]
     house_prices = house_prices.set_index('date')
-    house_prices.resample('M',how=np.mean)
+    house_prices.index = pd.to_datetime(house_prices.index, format='%Y.%m.%d')
+    house_prices = house_prices.resample('M').mean()
 
     house_prices_plot = house_prices.plot(title="House Price by Month",legend=None)
 
